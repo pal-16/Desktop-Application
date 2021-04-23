62fe3567d1a9ede78f6d925e0c8d903de75f61cf
pal-16 <50794716+pal-16@users.noreply.github.com>
2021-04-23 11:40:16 +0530

    added files for charity portal

---
+++ src/charitybank/Charitybank.java
@@ -0,0 +1,12 @@
+
+package charitybank;
+
+public class Charitybank {
+
+    public static void main(String[] args) {
+
+        homepage m = new homepage();
+        m.setVisible(true);
+    }
+
+}
